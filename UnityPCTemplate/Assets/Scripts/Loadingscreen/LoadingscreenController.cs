9b3b321244f85f5b7ee6921fc2704da9a465e623
Thomas van Opstal <opstal.thomas@gmail.com>
2016-12-17 15:48:49 +0100

    Splashscreen added LoadingController added

---
+++ UnityPCTemplate/Assets/Scripts/Loadingscreen/LoadingscreenController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+using System.Collections;
+
+public class LoadingscreenController : MonoBehaviour {
+
+    public enum Scenes {
+        SPLASH_SCREEN = 0,
+        LOADING_SCENE = 1,
+        MAIN_MENU = 2,
+        OPTIONS_MENU = 3,
+        GAME_SCENE = 4
+    }
+
+    private static Scenes sceneToBeLoaded;
+    private AsyncOperation async;
+
+    public Text loadingPercentageTextField;
+
+    public static void LoadScene(Scenes scene) {
+        sceneToBeLoaded = scene;
+        SceneManager.LoadScene((int)Scenes.LOADING_SCENE);
+    }
+
+    private void Start() {
+        StartCoroutine(LoadGame());
+    }
+
+    private void Update() {
+        if (async != null) {
+            loadingPercentageTextField.text = (Math.Ceiling(async.progress * 100)).ToString() + "%";
+            if (async.progress >= 0.90f) {
+                async.allowSceneActivation = true;
+            }
+        }
+    }
+
+    private IEnumerator LoadGame() {
+        async = SceneManager.LoadSceneAsync((int)sceneToBeLoaded);
+        async.allowSceneActivation = false;
+        yield return async;
+    }
+}

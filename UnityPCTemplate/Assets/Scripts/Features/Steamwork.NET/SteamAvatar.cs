df0d0b86b301283902adfd1fc198c017ee30d3bd
Thomas van Opstal <opstal.thomas@gmail.com>
2017-01-27 09:49:05 +0100

    [Added] - GetMediumSteamAvatar from SteamAvatar

---
+++ UnityPCTemplate/Assets/Scripts/Features/Steamwork.NET/SteamAvatar.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using Steamworks;
+
+namespace Features.Steam {
+    [DisallowMultipleComponent]
+    [RequireComponent(typeof(SteamManager))]
+    public class SteamAvatar : MonoBehaviour {
+        #region Public
+        public static SteamAvatar instance = null;
+        #endregion
+
+        private void OnAwake() {
+            #region Make sure only one instance exists
+            if (instance == null)
+                instance = this;
+            else if (instance != this)
+                Destroy(gameObject);
+
+            DontDestroyOnLoad(gameObject);
+            #endregion
+        }
+
+        public Texture2D GetSmallSteamAvatar() {
+            return new Texture2D(1, 1);
+        }
+
+        /// <summary>
+        /// Get the Medium sized(64x64) SteamAvatar of a specific user.
+        /// </summary>
+        /// <param name="steamID">CSteamID of the player</param>
+        /// <returns>SteamAvatar as Texture2D</returns>
+        public Texture2D GetMediumSteamAvatar(CSteamID steamID) {
+            if (SteamManager.Initialized) {
+                int avatarID = SteamFriends.GetMediumFriendAvatar(steamID);
+                uint imageWidth;
+                uint imageHeight;
+                bool success = SteamUtils.GetImageSize(avatarID, out imageWidth, out imageHeight);
+
+                if (success && imageWidth > 0 && imageHeight > 0) {
+                    byte[] image = new byte[imageWidth * imageHeight * 4];
+                    Texture2D avatarTexture = new Texture2D((int)imageWidth, (int)imageHeight, TextureFormat.RGBA32, false, true);
+                    success = SteamUtils.GetImageRGBA(avatarID, image, (int)(imageWidth * imageHeight * 4));
+
+                    if (success) {
+                        avatarTexture.LoadRawTextureData(image);
+                        avatarTexture.Apply();
+
+                        //Convert to PNG to increase quality
+                        byte[] avatarPNG = avatarTexture.EncodeToPNG();
+                        avatarTexture = null;
+                        avatarTexture = new Texture2D(1, 1);
+                        avatarTexture.LoadImage(avatarPNG);
+                    }
+
+                    return avatarTexture;
+                } else {
+                    Debug.Log("[SteamAvatar]: " + "Failed to get Image");
+                    return new Texture2D(0, 0);
+                }
+            } else {
+                Debug.Log("[SteamAvatar]: " + "SteamManager not Initialized");
+                return new Texture2D(0, 0);
+            }
+        }
+    }
+}

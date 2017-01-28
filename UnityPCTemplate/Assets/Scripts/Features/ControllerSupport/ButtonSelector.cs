0d62f189b17ddc4e59aaf73f39a005a1e2f1fbbe
Thomas van Opstal <opstal.thomas@gmail.com>
2017-01-28 19:38:22 +0100

    [Added] - ButtonSelector.cs: OnSceneLoad it will look for the EventSystem in the scene and set the first button in the scene hierachy as active [Use] - Place the Prefab in the splashscreen: Button Highlighted colors are still the same as non highlighted so change them first before testing. [Todo] - Only run this if the eventsystem does not have an First Active Button assigned [Todo] - Run this code when switching between keyboard/mouse and controller

---
+++ UnityPCTemplate/Assets/Scripts/Features/ControllerSupport/ButtonSelector.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+namespace Features.ControllerSupport
+{
+    /// <summary>
+    /// ButtonSelector.cs
+    /// <summary>
+    /// Author: Thomas van Opstal
+    /// <summary>
+    /// Everytime a new scene is loaded the first button in the scene will be set as selected in the eventsystem.
+    /// </summary>
+    [DisallowMultipleComponent]
+    public class ButtonSelector : MonoBehaviour
+    {
+        #region Public
+        public static ButtonSelector instance = null;
+        #endregion
+
+        #region Private
+        private EventSystem currentEventSystem; //EventSystem in the current Scene
+        #endregion
+
+        private void Awake() {
+            #region Make sure only one instance exists
+            if (instance == null)
+                instance = this;
+            else if (instance != this)
+                Destroy(gameObject);
+
+            DontDestroyOnLoad(gameObject);
+            #endregion
+        }
+
+        private void Start() {
+            //Get the first EventSystem on startup
+            currentEventSystem = GameObject.Find("EventSystem").GetComponent<EventSystem>();
+
+            if (currentEventSystem != null)
+                currentEventSystem.SetSelectedGameObject(GetFirstButtonInScene());
+        }
+
+        /// <summary>
+        /// This function is called when unity has loaded a scene. Get the EventSystem in the new scene and set the first button in the hierarchy.
+        /// </summary>
+        /// <param name="level">What level is Loaded?</param>
+        private void OnLevelWasLoaded(int level) {
+            //Get the new EventSystem in the newly loaded Scene
+            currentEventSystem = GameObject.Find("EventSystem").GetComponent<EventSystem>();
+
+            if (currentEventSystem != null)
+                currentEventSystem.SetSelectedGameObject(GetFirstButtonInScene());
+        }
+
+        /// <summary>
+        /// Look for the first active button in the hierarchy
+        /// </summary>
+        /// <returns>Return first active button in hierarchy if the scene has one</returns>
+        private GameObject GetFirstButtonInScene() {
+            Button firstButtonInScene = (Button)FindObjectOfType(typeof(Button));
+
+            if (firstButtonInScene != null)
+                return firstButtonInScene.gameObject;
+            else
+                return null;
+        }
+    }
+}

6377d06c75514b9b9a068db282d7bba50e01d698
Zammstein <sammie_8288@hotmail.com>
2017-01-29 16:53:37 +0100

    Refactored steam feature + integrated event system into it

---
+++ UnityPCTemplate/Assets/PCTemplate/Scripts/Features/Steam/SteamEventTypes.cs
@@ -0,0 +1,21 @@
+﻿namespace Features.Steam {
+
+    /// <summary>
+    /// SteamEventTypes
+    /// <summary>
+    /// Author: Sam Meyer
+    /// <summary>
+    /// Events used to communicate with the Steam feature. 
+    /// </summary>
+    public class SteamEventTypes {
+
+        /// <summary>
+        /// This event can be triggered to unlock a specific Steam achievement
+        /// passed as a string.
+        /// 
+        /// Argument(s) required:
+        /// 1. string: Achievement string id.
+        /// </summary>
+        public const string GLOBAL_STEAM_UNLOCK_ACHIEVEMENT = "GLOBAL_STEAM_UNLOCK_ACHIEVEMENT";
+    }
+}
